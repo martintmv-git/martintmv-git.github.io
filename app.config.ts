4c5246767312e3ba4fc05cc12e06f21cbf028f62
martintmv <work.martintmv@gmail.com>
2024-05-03 17:20:36 +0200

    nuxt blog import

---
+++ app.config.ts
@@ -0,0 +1,46 @@
+// https://github.com/nuxt-themes/alpine/blob/main/nuxt.schema.ts
+export default defineAppConfig({
+  alpine: {
+    title: 'Martin Tomov Blog',
+    description: 'Space for experiments with generative AI and machine learning.',
+    image: {
+      src: '/mtmv.png',
+      alt: 'Space for experiments with generative AI and machine learning.',
+      width: 400,
+      height: 300
+    },
+    header: {
+      position: 'left', // possible value are : | 'left' | 'center' | 'right'
+      logo: {
+        path: '/martintmv-dark.svg', // path of the logo
+        pathDark: '/martintmv.svg', // path of the logo in dark mode, leave this empty if you want to use the same logo
+        alt: 'martintmv', // alt of the logo
+        size: 'large'
+      }
+    },
+    footer: {
+      credits: {
+        enabled: false, // possible value are : true | false
+      },
+      navigation: false, // possible value are : true | false
+      alignment: 'left', // possible value are : 'none' | 'left' | 'center' | 'right'
+      message: '', // string that will be displayed in the footer (leave empty or delete to disable)
+      logo: {
+        path: '/martintmv-dark.svg', // path of the logo
+        pathDark: '/martintmv.svg', // path of the logo in dark mode, leave this empty if you want to use the same logo
+        alt: 'martintmv' // alt of the logo
+      }
+    },
+    socials: {
+      twitter: '#',
+      linkedin: {
+        icon: 'uil:linkedin',
+        label: 'LinkedIn',
+        href: 'https://www.linkedin.com/in/martintomov'
+      }
+    },
+    form: {
+      successMessage: 'Message sent. Thank you!'
+    }
+  }
+})
---
+++ tokens.config.ts
@@ -0,0 +1,12 @@
+import { defineTheme } from 'pinceau'
+
+export default defineTheme({
+    alpine: {
+      body: {
+        backgroundColor: {
+          initial: '#F5F5F5',
+          dark: '#0A0A0A',
+        }
+      }
+    }
+})
